ffe3daa2a7ab656ee9001bb52c65afb48fbe51d5
Victor Da Veiga <victor.veiga@unifesp.br>
2026-04-17 21:22:44 -0300

    Add C program to find the largest of 100 integers
    Implement program to read and find the largest of 100 positive integers.

---
+++ 1080b.c
@@ -0,0 +1,28 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Victor da Veiga Amaral Lopes
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/tournaments/rank/10175
+Data        : 17/04/2026
+Objetivo    : Ler e achar o maior de 100 números inteiros positivos
+Aprendizado : uso de vetores simples
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main()
+{
+    int n[100];
+    int i=0, maior=-1, v=-1;
+    for(i=0;i<100;i++)
+    scanf("%d", &n[i]);
+    i=0;
+    while(i<100){
+        if (n[i]>maior){
+            maior=n[i];
+            v=i;
+        }
+        i++;
+    }
+    printf("%d\n", maior);
+    printf("%d\n", v+1);
+}
