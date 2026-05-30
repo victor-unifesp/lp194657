e591cd7190bd40ff733f2791a9b126406a73bf06
Victor Da Veiga <victor.veiga@unifesp.br>
2026-05-30 12:27:51 -0300

    Update 1169.c

--- 1169.c
+++ 1169.c
@@ -8,26 +8,37 @@ Data        : 29/05/2026
 Objetivo    : achar o número de grãos em cada peça do tabuleiro
 Aprendizado : uso de linguagem recursiva e unsigned long long
 -------------------------------------------------------------------------- */
-
 #include <stdio.h>
 
-void resolver(int n) {
-    if (n == 0) return;
-
-    int x;
-    scanf("%d", &x);
-
-    unsigned long long graos = ((1ULL << (x - 1)) - 1) * 2 + 1;
-    unsigned long long kg = graos / 12000;
+unsigned long long resolver(int x) {
+    if (x == 0){
+     unsigned long long graos = 1;
+     return graos;    
+    }
     
-    printf("%llu kg\n", kg);
-
-    resolver(n - 1);
+    unsigned long long graos = 2*resolver(x-1);
+    
+    
+    return graos;
 }
 
 int main() {
-    int n;
+    int n, x;
+    unsigned long long g, kg;
     scanf("%d", &n);
-    resolver(n);
+    while(n>0){
+        scanf("%d", &x);
+        
+        if (x == 64) {
+            g = (resolver(63) - 1) * 2 + 1;
+        } else {
+        
+            g = resolver(x) - 1;
+        }
+        
+        kg = g / 12000;
+        printf("%llu kg\n", kg);
+        n--;
+    }
     return 0;
 }
